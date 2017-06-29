c77a5fa32e3fde26b9ecd10c31ee46f99b2d0dec
THUXEKO\ThuXeko <nguyenhathu2594@gmail.com>
2017-06-29 23:39:40 +0700

    Bắt đầu kết nối tới server, tạo config và api models

--- nodetodos/app.js
+++ nodetodos/app.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser"); //Đọc thông tin người dùng gửi về
 var morgan = require("morgan"); //Log request
-
+var mongoose = require("mongoose"); //Khai báo kết nối với mongoose
+var config = require("./config"); //Config để đọc được thông tin người dùng và lưu trữ sửa cho nhanh
 //Khởi tạo
 var app = express();
 var port = process.env.PORT || 3000; //Cấu hình theo biến môi trường(Nếu port k có sẽ gán mặc định cổng 3000)
+//Truy cập server
+mongoose.connect(config.getDbConnectionString());
 
 //Truy cập các middleware
 app.use("/assets", express.static(__dirname + "/public"));

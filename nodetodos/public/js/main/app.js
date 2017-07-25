4b0dbed5d3091553341191fee59095c4544ac8ae
nguyenhathu2594 <nguyenhathu2594@gmail.com>
2017-07-25 17:44:28 +0700

    Nâng cấp cho index có nút sửa(Chưa có chức năng)

--- nodetodos/public/js/main/app.js
+++ nodetodos/public/js/main/app.js
@@ -1,6 +1,35 @@
 //Khai báo angular
-var app = angular.module("app.todos", []); //Khai báo không phụ thuộc thư viện nào
+var app = angular.module("app.todos", ["xeditable"]); //Khai báo phụ thuộc -> [] thư viện nào
+
+app.run(function(editableOptions) {
+  editableOptions.theme = 'bs3';
+});
 
 app.controller("todoController", ['$scope', function ($scope) {
     $scope.appName = "Công việc";
+    $scope.formData = {};
+    $scope.todos = [
+        {
+            thongtin: "Khởi tạo dự án",
+            isDone: true
+        }, {
+            thongtin: "Cài đặt angular",
+            isDone: true
+        }, {
+            thongtin: "Kết nối angular vào db",
+            isDone: false
+        }
+        , {
+            thongtin: "Upload lên heroku",
+            isDone: false
+        }
+    ];
+    $scope.createTodo = function(){
+        var todo ={
+            thongtin: $scope.formData.thongtin,
+            isDone: false
+        }
+        $scope.todos.push(todo);
+        $scope.formData.thongtin = "";
+    }
 }]);
